f743ab9aae8e260909796551cae65ffd54d55af0
Marcin <marcinegzmont@gmail.com>
2023-09-15 19:30:04 +0100

    first commit

---
+++ src/Tours.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our title</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour tour={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
